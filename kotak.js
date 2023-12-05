519161dd1d669f8635dbd4972563cbd8b9e247ec
caraqu <97570905+ternakblog14@users.noreply.github.com>
2023-12-05 10:52:48 +0700

    Create kotak.js

---
+++ kotak.js
@@ -0,0 +1,15 @@
+atOptions = {
+
+		'key' : 'a224ca9984e8ccbc24183c8ce07947ab',
+
+		'format' : 'iframe',
+
+		'height' : 250,
+
+		'width' : 300,
+
+		'params' : {}
+
+	};
+
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/a224ca9984e8ccbc24183c8ce07947ab/invoke.js"></scr' + 'ipt>');
